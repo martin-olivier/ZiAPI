a079e3ee81a152d625ef9b456d30a498129a86b1
Martin Olivier <martin.olivier@live.fr>
2022-01-18 16:29:12 +0100

    fix: Windows unit tests

--- tests/IConfig.cpp
+++ tests/IConfig.cpp
@@ -10,13 +10,12 @@ struct position
 
 class Config : public ziapi::IConfig {
 private:
-    std::map<std::string, std::any> m_config_map;
+    std::map<std::string, std::any> m_config_map{};
 public:
     Config()
     {
-        m_config_map["mod_path"] = "./modules";
+        m_config_map["mod_path"] = std::string("./modules");
         m_config_map["std"] = 17;
-        m_config_map["ipv4"] = std::string("192.168.1.10");
         m_config_map["position"] = position{50, 100};
     }
     ~Config() = default;
@@ -34,9 +33,8 @@ TEST(IConfig, get_values)
 {
     Config conf;
 
-    ASSERT_EQ(std::any_cast<const char *>(conf["mod_path"]), "./modules");
+    ASSERT_EQ(std::any_cast<std::string>(conf["mod_path"]), "./modules");
     ASSERT_EQ(std::any_cast<int>(conf["std"]), 17);
-    ASSERT_EQ(std::any_cast<std::string>(conf["ipv4"]), "192.168.1.10");
     auto pos = std::any_cast<position>(conf["position"]);
     ASSERT_EQ(pos.x, 50);
     ASSERT_EQ(pos.y, 100);
@@ -46,11 +44,11 @@ TEST(IConfig, set_values)
 {
     Config conf;
 
+    conf["mod_path"] = std::string("/usr/local/mod");
+    ASSERT_EQ(std::any_cast<std::string>(conf["mod_path"]), "/usr/local/mod");
+
     conf["std"] = 20;
     ASSERT_EQ(std::any_cast<int>(conf["std"]), 20);
-
-    conf["ipv4"] = std::string("1.1.1.1");
-    ASSERT_EQ(std::any_cast<std::string>(conf["ipv4"]), "1.1.1.1");
 }
 
 TEST(IConfig, bad_key)
