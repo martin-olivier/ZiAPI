89ccf68cf4a6119e827429e9275ac551c077303d
Martin Olivier <martin.olivier@live.fr>
2022-01-22 18:21:17 +0100

    fix: logger

--- include/ziapi/Logger.hpp
+++ include/ziapi/Logger.hpp
@@ -29,17 +29,17 @@ enum class LogType : uint8_t {
 inline void Logger(const std::string &message, LogType log_type = LogType::INFO, std::ostream &stream = std::cout)
 {
     static const std::map<LogType, std::string> log_type_map{
-        {LogType::INFO, color::BLUE + std::string("[i] ") + color::DEFAULT},
-        {LogType::WARNING, color::YELLOW + std::string("[!] ") + color::DEFAULT},
-        {LogType::ERROR, color::RED + std::string("[X] ") + color::DEFAULT},
-        {LogType::DEBUG, color::GREEN + std::string("[&] ") + color::DEFAULT},
+        {LogType::INFO, color::BLUE + std::string(" [i] ") + color::DEFAULT},
+        {LogType::WARNING, color::YELLOW + std::string(" [!] ") + color::DEFAULT},
+        {LogType::ERROR, color::RED + std::string(" [X] ") + color::DEFAULT},
+        {LogType::DEBUG, color::GREEN + std::string(" [&] ") + color::DEFAULT},
     };
 
     time_t actual_time = std::time(nullptr);
     std::string time_str = std::ctime(&actual_time);
     time_str.erase(std::remove_if(time_str.begin(), time_str.end(), [](char a) { return a == '\n'; }), time_str.end());
 
-    stream << '[' << time_str << ']';
+    stream << color::CYAN << time_str << color::DEFAULT;
     stream << log_type_map.at(log_type) << message << color::DEFAULT << std::endl;
 }
 
