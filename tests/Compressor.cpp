8793ae9872b1216aca591f3e66ce711e3ed7e90f
Allan <allan.debeve@epitech.eu>
2022-01-23 19:15:23 +0100

    feat: add test and end of compressor module

--- tests/Compressor.cpp
+++ tests/Compressor.cpp
@@ -1,6 +1,6 @@
-#include "examples/modules/CompressorModule.hpp"
-
 #include <gtest/gtest.h>
+
+#include "compressor/CompressorModule.hpp"
 
 TEST(Compressor, UtilsInfo)
 {
@@ -8,23 +8,23 @@ TEST(Compressor, UtilsInfo)
 
     ASSERT_EQ(compressor.GetName(), "CompressorModule");
     ASSERT_EQ(compressor.GetDescription(), "Compress the response body before sending it back to the network");
-    ASSERT_EQ(compressor.GetCompatibleApiVersion(), Version(1, 0));
-    ASSERT_EQ(compressor.GetVersion(), Version(1, 0));
+    ASSERT_EQ(compressor.GetCompatibleApiVersion(), (ziapi::Version{1, 0}));
+    ASSERT_EQ(compressor.GetVersion(), (ziapi::Version{1, 0}));
 }
 
 TEST(Compressor, compressionRate)
 {
     CompressorModule compressor;
     ziapi::http::Context ctx;
-    ziapi::http::Response res;
-
-    res.version = 11;
-    res.status_code = 200;
-    res.reason = std::string("OK");
+    ziapi::http::Response res = {
+        .version = 11,
+        .status_code = 200,
+        .reason = std::string("OK"),
+        .fields = std::map<std::string, std::string>({}),
+        .body = std::string("not compressed stuff blabla omg so long"),
+    };
     res.fields.insert(std::make_pair<std::string, std::string>("Content-Type", "application/json"));
-    res.body = std::string("not compressed stuff blabla omg so long");
 
     compressor.PostProcess(ctx, res);
-    ASSERT_EQ(res.body, "hihihi i'm compressed bro");
+    ASSERT_EQ(res.body, "not compressed stuf");
 }
-
