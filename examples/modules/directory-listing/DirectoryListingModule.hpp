ddb21fd19e23c1af0bef518f80ac02819aea4f39
Diego ROJAS <63368264+rojasdiegopro@users.noreply.github.com>
2022-01-23 21:55:48 +0100

    fix: add missing override qualifiers in directory listing

--- examples/modules/directory-listing/DirectoryListingModule.hpp
+++ examples/modules/directory-listing/DirectoryListingModule.hpp
@@ -5,33 +5,36 @@
 
 class DirectoryListingModule : public ziapi::IHandlerModule {
 public:
-    void Init(const ziapi::Config &cfg)
+    void Init(const ziapi::Config &cfg) override
     {
         /// In our config, we can specify which folder our module serves.
         root_ = std::any_cast<std::string>(cfg.at("modules.directory_listing.path"));
     }
 
-    [[nodiscard]] ziapi::Version GetVersion() const noexcept { return {1, 0}; }
+    [[nodiscard]] ziapi::Version GetVersion() const noexcept override { return {1, 0}; }
 
-    [[nodiscard]] ziapi::Version GetCompatibleApiVersion() const noexcept { return {1, 0}; }
+    [[nodiscard]] ziapi::Version GetCompatibleApiVersion() const noexcept override { return {1, 0}; }
 
-    [[nodiscard]] const char *GetName() const noexcept { return "DirectoryListing"; }
+    [[nodiscard]] const char *GetName() const noexcept override { return "DirectoryListing"; }
 
-    [[nodiscard]] const char *GetDescription() const noexcept { return "Give access to a filesystem over HTTP"; }
+    [[nodiscard]] const char *GetDescription() const noexcept override
+    {
+        return "Give access to a filesystem over HTTP";
+    }
 
-    [[nodiscard]] double GetHandlerPriority() const noexcept
+    [[nodiscard]] double GetHandlerPriority() const noexcept override
     {
         /// Our module doesn't have any specific priority requirements.
         return 0.5f;
     }
 
-    [[nodiscard]] bool ShouldHandle(const ziapi::http::Context &ctx, const ziapi::http::Request &req) const
+    [[nodiscard]] bool ShouldHandle(const ziapi::http::Context &ctx, const ziapi::http::Request &req) const override
     {
         /// We only want to handle GET requests.
         return req.method == ziapi::http::method::GET;
     }
 
-    void Handle(ziapi::http::Context &ctx, const ziapi::http::Request &req, ziapi::http::Response &res)
+    void Handle(ziapi::http::Context &ctx, const ziapi::http::Request &req, ziapi::http::Response &res) override
     {
         /// Here we concat the root path from our config (e.g /var/www/) with the target from our request (e.g.
         /// index.html) to get the full path of our file.
