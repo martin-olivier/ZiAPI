29ce2a2992a7ec6fc4fdb310d3ada98119061a15
Arthur Vasseur <arthur.vasseur@epitech.eu>
2022-02-02 14:55:37 +0100

    feat: changing the prototype of Pop() function of the IResponseInputQueue interface

--- include/ziapi/Http.hpp
+++ include/ziapi/Http.hpp
@@ -59,7 +59,7 @@ class IResponseInputQueue {
 public:
     virtual ~IResponseInputQueue() = default;
 
-    virtual std::pair<Request, Context> Pop() = 0;
+    virtual bool Pop(std::pair<Request, Context> &req) = 0;
 
     [[nodiscard]] virtual std::size_t Size() const noexcept = 0;
 };
